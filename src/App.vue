932d30ca09dc2cd2f5e7fa9a337fcb0b876a1c6d
DZF <dengzhifeng_63@163.com>
2019-03-22 18:54:45 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <tabbar class="vux-demo-tabbar" icon-class="vux-center"  slot="bottom">
+      <tabbar-item :link="{path:'/'}">
+        <span class="demo-icon-22 vux-demo-tabbar-icon-home" slot="icon" style="position:relative;top: -2px;">
+          <img src="../src/assets/icon/home.png" alt="home">
+        </span>
+        <span class="demo-icon-22" slot="icon-active">
+          <img src="../src/assets/icon/homeActive.png" alt="home">
+        </span>
+        <span slot="label">首页</span>
+      </tabbar-item>
+      <tabbar-item :link="{path:'/'}" badge="9">
+        <span class="demo-icon-22" slot="icon">
+          <img src="../src/assets/icon/attention.png" alt="home">
+        </span>
+        <span class="demo-icon-22" slot="icon-active">
+          <img src="../src/assets/icon/attentionActive.png" alt="home">
+        </span>
+        <span slot="label">
+          <!--<span v-if="componentName" class="vux-demo-tabbar-component">{{componentName}}</span>-->
+          <!--<span v-else>活动</span>-->
+          活动
+        </span>
+      </tabbar-item>
+      <tabbar-item :link="{path:'/'}" >
+        <span class="demo-icon-22 vux-demo-tabbar-icon-home" slot="icon" style="position:relative;top: -2px;">
+          <img src="../src/assets/icon/message.png" alt="home">
+        </span>
+        <span class="demo-icon-22" slot="icon-active">
+          <img src="../src/assets/icon/messageActive.png" alt="home">
+        </span>
+        <span slot="label">首页</span>
+      </tabbar-item>
+      <tabbar-item :link="{path:'/'}" >
+        <span class="demo-icon-22 vux-demo-tabbar-icon-home" slot="icon" style="position:relative;top: -2px;">
+          <img src="../src/assets/icon/my.png" alt="home">
+        </span>
+        <span class="demo-icon-22" slot="icon-active">
+          <img src="../src/assets/icon/myActive.png" alt="home">
+        </span>
+        <span slot="label">首页</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+  import { XHeader, Tabbar, TabbarItem } from 'vux'
+  export default {
+    name: 'app',
+    components: {
+      XHeader,
+      Tabbar,
+      TabbarItem
+    }
+  }
+</script>
+
+<style lang="less">
+@import '../src/assets/style/reset';
+body {
+  background-color: #fbf9fe;
+}
+</style>
---
+++ src/components/HelloFromVux.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <x-header class="ignore" :left-options="{showBack: false}">主页</x-header>
+    <swiper :list="list"></swiper>
+    <!--<group title="cell demo">-->
+      <!--<cell title="VUX" value="cool" is-link></cell>-->
+    <!--</group>-->
+    <group>
+      <x-input title="title" v-model="value"></x-input>
+    </group>
+    <div class="font22 box">哈哈啥时候</div>
+  </div>
+</template>
+
+<script>
+import { Group, Cell, XHeader, Swiper, XInput } from 'vux'
+
+export default {
+  components: {
+    Group,
+    Cell,
+    XHeader,
+    Swiper,
+    XInput
+  },
+  data () {
+    return {
+      value: '',
+      msg: 'Hello World!',
+      list: [{
+        url: 'javascript:',
+        img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2622829061,2227432350&fm=26&gp=0.jpg',
+        title: '我的'
+      }, {
+        url: 'javascript:',
+        img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=685006216,2192383313&fm=26&gp=0.jpg',
+        title: '都是我的',
+        fallbackImg: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2622829061,2227432350&fm=26&gp=0.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style  lang="less">
+  .vux-demo {
+    text-align: center;
+  }
+  .logo {
+    width: 100px;
+    height: 100px
+  }
+</style>
