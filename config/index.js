0047cf307f49cd047504bdc0453f7e6fe3706e55
DZF <dengzhifeng_63@163.com>
2019-04-03 19:30:03 +0800

    测试

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://love.ufutx.com/mobile/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
