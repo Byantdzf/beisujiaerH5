21acbd6f7d638430e0a82d0e4d8949cf635b68aa
DZF <dengzhifeng_63@163.com>
2019-04-08 18:43:33 +0800

    测试

--- src/pages/user/activityDetail.vue
+++ src/pages/user/activityDetail.vue
@@ -152,57 +152,29 @@
           //   }
           // })
           // }
-          $toastWarn(wxconfig.appId)
-
-          function onBridgeReady () {
-            WeixinJSBridge.invoke(
-              'getBrandWCPayRequest', {
-                'appId': 'wx2421b1c4370ec43b',     // 公众号名称，由商户传入
-                'timeStamp': '1395712654',         // 时间戳，自1970年以来的秒数
-                'nonceStr': 'e61463f8efa94090b1f366cccfbbb444', // 随机串
-                'package': 'prepay_id=u802345jgfjsdfgsdg888',
-                'signType': 'MD5',         // 微信签名方式：
-                'paySign': '70EA570631E4BB79628FBCA90534C63FF7FADD89' // 微信签名
-              },
-              function (res) {
-                if (res.err_msg === 'get_brand_wcpay_request:ok') {
-                  // 使用以上方式判断前端返回,微信团队郑重提示：
-                  // res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+          this.$wechat.onMenuShareTimeline({
+            title: wxconfig.appId
+          })
+          // $toastWarn(wxconfig.appId)
+          vm.$wechat.chooseWXPay({
+            appId: wxconfig.appId,
+            timestamp: wxconfig.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+            nonceStr: wxconfig.nonceStr, // 支付签名随机串，不长于 32 位
+            package: wxconfig.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+            signType: wxconfig.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+            paySign: wxconfig.paySign, // 支付签名
+            success: function (res) {
+              vm.$router.push({
+                name: 'activityPaySuccess',
+                params: {
+                  id: this.id
                 }
               })
-          }
-
-          if (typeof WeixinJSBridge === 'undefined') {
-            if (document.addEventListener) {
-              document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false)
-            } else if (document.attachEvent) {
-              document.attachEvent('WeixinJSBridgeReady', onBridgeReady)
-              document.attachEvent('onWeixinJSBridgeReady', onBridgeReady)
+            },
+            fail: function (res) {
+              console.log(res)
             }
-          } else {
-            onBridgeReady()
-          }
-          // vm.$wechat.chooseWXPay({
-          //   appId: 'wxc41491431733671e',
-          //   timestamp: wxconfig.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-          //   nonceStr: wxconfig.nonceStr, // 支付签名随机串，不长于 32 位
-          //   package: wxconfig.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-          //   signType: wxconfig.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-          //   paySign: wxconfig.paySign, // 支付签名
-          //   success: function (res) {
-          //     vm.$router.push({
-          //       name: 'activityPaySuccess',
-          //       params: {
-          //         id: this.id
-          //       }
-          //     })
-          //   },
-          //   fail: function (res) {
-          //     console.log(res)
-          //   }
-          // })
-          // }
-          // console.log(vm.$wechat.chooseWXPay())
+          })
         }).catch((error) => {
           console.log(error)
         })
