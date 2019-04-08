8b6e9e372196d714e4d4d420e7aadcdd7a4966f8
DZF <dengzhifeng_63@163.com>
2019-04-08 11:48:57 +0800

    测试

--- src/config/api.js
+++ src/config/api.js
@@ -37,11 +37,7 @@ const api = () => {
     if (response.status === 200 && response.data.code === 2) { // token过期
       // localStorage.removeItem('ACCESS_TOKEN')
       console.log(window.location.pathname)
-      debugger
-      
       window.location.href = `${window.location.pathname}register`
-      debugger
-
       // this.$router.push({
       //       //   name: 'register'
       //       // })
