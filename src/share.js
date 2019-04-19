f03d5872a32f1cae4f75ed8a627d096800cce7cc
DZF <dengzhifeng_63@163.com>
2019-04-19 15:57:24 +0800

    测试

--- src/share.js
+++ src/share.js
@@ -94,21 +94,20 @@ function wxInit (res) {
     jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline']
   })
   wx.ready(function () {
-    // ------------------------
-    // wx.onMenuShareAppMessage({
-    //   title: title, // 分享标题
-    //   desc: desc, // 分享描述
-    //   link: links, // 分享链接
-    //   imgUrl: imgUrl, // 分享图标
-    //   success: function () {
-    //     alert('分享成功')
-    //     _this.showFx = false
-    //   },
-    //   cancel: function () {
-    //     alert('分享失败')
-    //     _this.showFx = false
-    //   }
-    // })
+    wx.onMenuShareAppMessage({
+      title: title, // 分享标题
+      desc: desc, // 分享描述
+      link: links, // 分享链接
+      imgUrl: imgUrl, // 分享图标
+      success: function () {
+        alert('分享成功')
+        _this.showFx = false
+      },
+      cancel: function () {
+        alert('分享失败')
+        _this.showFx = false
+      }
+    })
     // 微信分享菜单测试
     // wx.onMenuShareTimeline({
     //   title: title, // 分享标题
@@ -123,47 +122,7 @@ function wxInit (res) {
     //     alert('分享失败')
     //     _this.isShow = true
     //   }
-    // })=================
-
-    var link = window.location.href
-    var protocol = window.location.protocol
-    var host = window.location.host
-    // 分享朋友圈
-    console.log('分享朋友圈')
-    wx.onMenuShareTimeline({
-      title: '这是一个自定义的标题！',
-      link: link,
-      imgUrl: imgUrl, // 自定义图标
-      trigger: function (res) {
-        // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回.
-        // alert('click shared');
-      },
-      success: function (res) {
-        // alert('shared success');
-        // some thing you should do
-      },
-      cancel: function (res) {
-        // alert('shared cancle');
-      },
-      fail: function (res) {
-        // alert(JSON.stringify(res));
-      }
-    })
-    // 分享给好友
-    wx.onMenuShareAppMessage({
-      title: '这是一个自定义的标题！', // 分享标题
-      desc: '这是一个自定义的描述！', // 分享描述
-      link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-      imgUrl: imgUrl, // 自定义图标
-      type: 'link', // 分享类型,music、video或link，不填默认为link
-      dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-      success: function () {
-        // 用户确认分享后执行的回调函数
-      },
-      cancel: function () {
-        // 用户取消分享后执行的回调函数
-      }
-    })
+    // })
   })
   wx.error(function (err) {
     alert(JSON.stringify(err))
