ef787e30ec7a1daa4bc12910c1fe8b204aa3869b
DZF <dengzhifeng_63@163.com>
2019-04-19 16:41:34 +0800

    测试

--- src/share.js
+++ src/share.js
@@ -5,12 +5,70 @@ exports.install = function (Vue, options) {
   Vue.prototype.$shareList = function (imgUrl, link, desc, title) {
     // 分享
     let vm = this
-    console.log(this)
     // let url = encodeURIComponent(location.href.split('#')[0])
     let url = location.href.toString()
     let data = {url: url}
     vm.$http.post(`/official/js/config`, data).then(({data}) => {
-      wxInit(data, imgUrl, link, desc, title)
+      // wxInit(data, imgUrl, link, desc, title)
+      wx.config({
+        debug: false, // true:调试时候弹窗
+        appId: data.appId, // 微信appid
+        timestamp: data.timestamp, // 时间戳
+        nonceStr: data.nonceStr, // 随机字符串
+        signature: data.signature, // 签名
+        jsApiList: [
+          // 所有要调用的 API 都要加到这个列表中
+          'onMenuShareTimeline', // 分享到朋友圈接口
+          'onMenuShareAppMessage', //  分享到朋友接口
+          'onMenuShareQQ', // 分享到QQ接口
+          'onMenuShareWeibo' // 分享到微博接口
+        ]
+      })
+      wx.checkJsApi({
+        jsApiList: [
+          // 所有要调用的 API 都要加到这个列表中
+          'onMenuShareTimeline', // 分享到朋友圈接口
+          'onMenuShareAppMessage', //  分享到朋友接口
+          'onMenuShareQQ', // 分享到QQ接口
+          'onMenuShareWeibo' // 分享到微博接口
+        ],
+        success: function (res) {
+        }
+      })
+
+      wx.ready(function () {
+        // 微信分享的数据
+        var shareData = {
+          imgUrl: imgUrl, // 分享显示的缩略图地址
+          link: link, // 分享地址
+          desc: desc, // 分享描述
+          title: title, // 分享标题
+          success: function () {
+            // 分享成功可以做相应的数据处理
+            alert('分享成功')
+            alert('appId:' + res.appId)
+            alert('timestamp:' + res.timestamp)
+            alert('nonceStr:' + res.nonceStr)
+            alert('signature:' + res.signature)
+          },
+          fail: function () {
+            alert('调用失败')
+          },
+          complete: function () {
+            alert('调用结束')
+          }
+        }
+        wx.updateTimelineShareData(shareData)
+        wx.updateAppMessageShareData(shareData)
+        wx.onMenuShareQQ(shareData)
+        wx.onMenuShareWeibo(shareData)
+      })
+      wx.error(function (res) {
+        // config信息验证失败会执行error函数，如签名过期导致验证失败，
+        // 具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，
+        // 对于SPA可以在这里更新签名。
+        alert('分享失败')
+      })
     }).catch((error) => {
       console.log(error)
     })
